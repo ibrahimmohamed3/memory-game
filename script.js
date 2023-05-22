168519bf5997395613e0cf46cb2882fee172afcc
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-05-22 12:27:16 +0300

    update changes

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 let counter = 0;
-let firstSelection = "1";
-let secondSelection ="2";
+let firstSelection = "";
+let secondSelection ="";
 
-const cards = document.querySelectorAll(".crads .card");
+const cards = document.querySelectorAll(".cards .card");
 cards.forEach((card) => {
     card.addEventListener("click",() => {
     card.classList.add("clicked");
+   if(counter === 0) {
+    firstSelection=
+    card.getAttribute("image");
+    counter++;
+   }else {
+    secondSelection=
+    card.getAttribute("image");
+    counter = 0;
+    if(firstSelection === secondSelection) {
+        const correctCards =
+        document.querySelectorAll(
+            ".card[image=`" + firstSelection + "`]"
+        );
+        correctCards[0].classList.add("checked");
+        correctCards[0].classList.remove("clicked");
+        correctCards[1].classList.add("checked");
+        correctCards[1].classList.remove("clicked");
+} else {
+    const incorrectCards = document.querySelectorAll(".card.clicked");
+    incorrectCards[0].classList.add("shake");
+    incorrectCards[1].classList.add("shake");
+    setTimeout(() => {
+        incorrectCards[0].classList.remove("shake");
+        incorrectCards[0].classList.remove("cliked");
+        incorrectCards[1].classList.remove("shake");
+        incorrectCards[1].classList.remove("cliked");
+    }, 800);
+        
     
+}
+   }
 });
 });
