019d44454c79d9f40b6533e1f2379cfb4d78cfff
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-05-24 16:04:06 +0300

    add changes

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 let counter = 0;
 let firstSelection = "";
-let secondSelection ="";
+let secondSelection = "";
 
 const cards = document.querySelectorAll(".cards .card");
 cards.forEach((card) => {
-    card.addEventListener("click",() =>{
+  card.addEventListener("click", () => {
     card.classList.add("clicked");
-   if(counter === 0){
-    firstSelection=
-    card.getAttribute("image");
-    counter++;
-   }else {
-    secondSelection=
-    card.getAttribute("image");
-    counter = 0;
-    if(firstSelection === secondSelection) {
-        const correctCards =
-        document.querySelectorAll(
-            ".card[image=`" + firstSelection + "`]"
+
+    if (counter === 0) {
+      firstSelection = card.getAttribute("log");
+      counter++;
+    } else {
+      secondSelection = card.getAttribute("log");
+      counter = 0;
+
+      if (firstSelection === secondSelection) {
+        const correctCards = document.querySelectorAll(
+          ".card[log='" + firstSelection + "']"
         );
+
         correctCards[0].classList.add("checked");
         correctCards[0].classList.remove("clicked");
         correctCards[1].classList.add("checked");
         correctCards[1].classList.remove("clicked");
-} else {
-    const incorrectCards = document.querySelectorAll(".card.clicked");
-    incorrectCards[0].classList.add("shake");
-    incorrectCards[1].classList.add("shake");
-    setTimeout(() =>{
-        incorrectCards[0].classList.remove ("shake");
-        incorrectCards[0].classList.remove ("cliked");
-        incorrectCards[1].classList.remove ("shake");
-        incorrectCards[1].classList.remove ("cliked");
-    }, 800); 
-}
-   }
-});
+      } else {
+        const incorrectCards = document.querySelectorAll(".card.clicked");
+
+        incorrectCards[0].classList.add("shake");
+        incorrectCards[1].classList.add("shake");
+
+        setTimeout(() => {
+          incorrectCards[0].classList.remove("shake");
+          incorrectCards[0].classList.remove("clicked");
+          incorrectCards[1].classList.remove("shake");
+          incorrectCards[1].classList.remove("clicked");
+        }, 800);
+      }
+    }
+  });
 });
